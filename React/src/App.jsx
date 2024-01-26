145f6ba8d728688a40bc1bf5fc902eeff109628e
Belhadj Ahmed Walid <belhadj.ahmedwalid1@gmail.com>
2024-01-26 16:01:01 +0100

    Added minimal React app for WebComponent

---
+++ React/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../Components/ImageCompareComponent.js";
+
+
+export default () => (
+  
+ <>
+  <image-compare label-text="Screen Reader Label Text">
+  <img slot="image-1" alt="Alt Text" src="https://image-compare-component.netlify.app/assets/images/example-image.svg"/>
+  <img slot="image-2" alt="Alt text" src="https://image-compare-component.netlify.app/assets/images/example-image2.svg"/>
+</image-compare>
+ </>
+);
