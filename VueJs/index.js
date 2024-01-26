f6e7578c1696b1bd4928476d0cd9bd52bbf980f4
Belhadj Ahmed Walid <belhadj.ahmedwalid1@gmail.com>
2024-01-26 16:19:42 +0100

    Update VueJS index.html and index.js files

--- VueJs/index.js
+++ VueJs/index.js
@@ -3,7 +3,6 @@ var mySimpleApp = new Vue({
     el: '#mainapp',
     template: `
     <div id="app-wrapper">
-          <h2 id="app-title">I don't think a Vue app can be much simpler than this!</h2>
           <image-compare label-text="Screen Reader Label Text">
           <img slot="image-1" alt="Alt Text" src="https://image-compare-component.netlify.app/assets/images/example-image.svg"/>
           <img slot="image-2" alt="Alt text" src="https://image-compare-component.netlify.app/assets/images/example-image2.svg"/>
